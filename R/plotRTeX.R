90eadab2524320a6bbce72850e8d4b5bf6472378
egallic <ewen.gallic@gmail.com>
2016-05-23 12:59:41 +0200

    ggplot2 to PDF in LaTeX Format
    Converts ggplot2 graphics to PDF using LaTeX. Relies on tikzDevice to
    create a tikzpicture and then compiles a LaTeX standalone document
    using PDFLaTeX.

---
+++ R/plotRTeX.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tikzDevice)
+
+# Exports ggplot2 to a tex and then runs pdflatex to a standalone tex file
+# @plot : ggplot2 object
+# @path : path to file that will be created (must not contain filename)
+# @filename: name of the file (without extension)
+# @width, height: plot dimensions
+# path <- "../images/" ; filename <- "data" ; width = 15 ; height = .75*width
+ggplot2_to_pdf <- 
+  function(plot, path = "./", filename, width = 15, height = 15){
+    content <- paste0("\\documentclass{standalone}
+                      \\usepackage{amssymb}
+                      \\usepackage{pgfplots}
+                      \\usetikzlibrary{pgfplots.groupplots}
+                      \\definecolor{mygrey2}{RGB}{127,127,127}
+                      \\definecolor{deepblue}{RGB}{0,129,188}
+                      \\definecolor{deepgreen}{RGB}{0,157,87}
+                      \\definecolor{deepred}{RGB}{238,50,78}
+                      \\begin{document}
+                      
+                      \\input{",
+                      path, filename,
+                      "_content.tex}
+                      
+                      \\end{document}")
+    
+    # Le fichier qui va importer le graphique au format tex pour le compiler en pdf
+    fileConn<-file(paste0(path, filename, ".tex"))
+    writeLines(content, fileConn)
+    close(fileConn)
+    
+    # Exportation du graphique en tex
+    tikz(file = paste0(path, filename, "_content.tex"), width = width, height = height)
+    print(plot)
+    dev.off()
+    
+    # Executer le fichier tex pour obtenir le pdf
+    system(paste0("/Library/TeX/texbin/pdflatex -shell-escape -synctex=1 -interaction=nonstopmode  ",
+                  path, filename, ".tex"))
+    system(paste0("mv ", filename, ".pdf ", path))
+    system(paste0("rm ", filename, ".aux"))
+    system(paste0("rm ", filename, ".log"))
+    system(paste0("rm ", filename, ".synctex.gz"))
+}
+
