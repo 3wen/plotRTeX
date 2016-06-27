130abc240a655ac936ccf0218abdd3184acdcc0c
egallic <ewen.gallic@gmail.com>
2016-06-27 11:59:41 +0200

    Removing TeX files
    Added the possibility to keep or remove intermediate tex files.

--- R/plotRTeX.R
+++ R/plotRTeX.R
@@ -8,10 +8,12 @@ library(tikzDevice)
 # @interpreter : which interpreter to use (default to pdflatex)
 # @path : path to file that will be created (must not contain filename)
 # @filename: name of the file (without extension)
+# @keep_tex: if FALSE (default), removes TeX files
 # @width, height: plot dimensions
 # path <- "../images/" ; filename <- "data" ; width = 15 ; height = .75*width
 ggplot2_to_pdf <- 
-  function(plot, interpreter = "pdflatex", path = "./", filename, width = 15, height = 15){
+  function(plot, interpreter = "pdflatex", path = "./",
+           filename, keep_tex = FALSE, width = 15, height = 15){
     content <- paste0("\\documentclass{standalone}
                       \\usepackage{amssymb}
                       \\usepackage{pgfplots}
@@ -45,5 +47,9 @@ ggplot2_to_pdf <-
     system(paste0("rm ", filename, ".aux"))
     system(paste0("rm ", filename, ".log"))
     system(paste0("rm ", filename, ".synctex.gz"))
+    if(!keep_tex){
+      system(paste0("rm ", path, filename, ".tex"))
+      system(paste0("rm ", path, filename, "_content.tex"))
+    }
 }
 
